bedaf143793ccb75dde8edee0af2a197e0124aab
AlexanderAV96 <alenn@ya.ru>
2023-03-10 16:40:00 +0300

    thread new8

--- third/thread.cpp
+++ third/thread.cpp
@@ -18,7 +18,9 @@ using Counter = std::map<std::string, std::size_t>;
 
 std::string tolower(const std::string& str);
 
-void count_words(std::istream *stream, Counter&);
+void count_words(std::istream& stream, Counter&);
+
+Counter count_wordsnew( std::istream* stream , Counter );
 
 void print_topk(std::ostream& stream, const Counter&, const size_t k);
 
@@ -33,20 +35,20 @@ int main(int argc, char* argv[]) {
     std::vector<std::thread> thr;
     std::vector<std::ifstream> str;
     std::vector <std::unique_ptr<std::ifstream>> ptr;
-    std::vector<std::future <std::ifstream>> thr1;
-
+    std::vector<std::future <Counter>> thr1;
+    std::vector<std::future <void>> thr2;
 
     auto start = std::chrono::high_resolution_clock::now();
     Counter freq_dict;
     for (int i = 1; i < argc; ++i) {
       
         
-          // std::ifstream input{ argv[i] };
+           std::ifstream input{ argv[i] };
 
         ptr.emplace_back(std::make_unique<std::ifstream>((argv[i])));
 
-      //   if (!input.is_open()) {
-        if (!ptr.back().get()->is_open()) {
+         if (!input.is_open()) {
+       // if (!ptr.back().get()->is_open()) {
             std::cerr << "Failed to open file " << argv[i] << '\n';
             return EXIT_FAILURE;
         }
@@ -58,26 +60,34 @@ int main(int argc, char* argv[]) {
         
                                   // ptr.emplace_back(std::make_unique<std::ifstream>((argv[i])));
         
-                                 //  std::thread th(count_words, ref(str.back()), ref(freq_dict));
+                            //       std::thread th(count_words, std::move(str.back()), ref(freq_dict));
         
-        thr.emplace_back(count_words, (ptr.back().get()), ref(freq_dict));
-       
-                                 // thr[i-1].join();
-        
+      //  thr.emplace_back(count_words, (ptr.back().get()), ref(freq_dict));
+      
+            //                      thr[i-1].join();
+      //  thr2.emplace_back( std::async( std::launch::async , count_words , ( ptr.back( ).get( ) ) , ref( freq_dict ) ) );
 
-      //  thr1.emplace_back(std::launch::async, count_words, (ptr.back().get()), ref(freq_dict))
+      
 
        
         
        
 ////////////////////////////////////////////////
-      // count_words(input, freq_dict);
+                                                                 count_words(input, freq_dict);
     }
     //////////////////////////////////////
-     for (auto& i : thr) i.join();
+   // for (auto& i : thr) i.join();
 
 
     ////////////////////////////
+
+    //for ( auto& i : thr2 ) i.get( );
+        
+    
+
+
+    ///////////////////////////
+
     print_topk(std::cout, freq_dict, TOPK);
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
@@ -92,14 +102,28 @@ std::string tolower(const std::string& str) {
     return lower_str;
 };
 
-void count_words(std::istream *stream, Counter& counter) {
+void count_words(std::istream &stream, Counter& counter) {
    
     
-    std::for_each(std::istream_iterator<std::string>(*stream),
+    std::for_each(std::istream_iterator<std::string>(stream),
         std::istream_iterator<std::string>(),
         [&counter](const std::string& s) { std::lock_guard guard(mtx);++counter[tolower(s)]; });
     
 }
+
+Counter count_wordsnew( std::istream* stream , Counter counter )
+{
+
+
+    std::for_each( std::istream_iterator<std::string>( *stream ) ,
+                   std::istream_iterator<std::string>( ) ,
+                   [&counter] ( const std::string& s ) { std::lock_guard guard( mtx ); ++counter [ tolower( s ) ]; } );
+
+
+    return counter;
+}
+
+
 
 void print_topk(std::ostream& stream, const Counter& counter, const size_t k) {
     std::vector<Counter::const_iterator> words;
